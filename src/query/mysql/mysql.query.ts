56132ff439b0ee99443998f6c23aa1397af072fc
Udhaya-Phx <udhaya@phoenixtechnologies.io>
2025-03-27 16:12:28 +0530

    sql fix

--- src/query/mysql/mysql.query.ts
+++ src/query/mysql/mysql.query.ts
@@ -1,11 +1,17 @@
 export const updateMissingTransaction = () => {
-  return `UPDATE missing_transaction 
+  return `UPDATE missing_transaction_test_1 
   SET charge_query = ?,
   charge_event_query = ?,
   execution_status = ?
   WHERE customer_id = ? `;
 };
 
+export const updateMissingTransactionStatus = () => {
+  return `UPDATE missing_transaction_test_1 
+  SET execution_status = ?
+  WHERE customer_id = ? `;
+};
+
 export const getMissingTransaction = (status: string) => {
-  return `SELECT * FROM missing_transaction WHERE execution_status = '${status}'`;
+  return `SELECT * FROM missing_transaction_test_1 WHERE execution_status = '${status}' limit 1`;
 };
