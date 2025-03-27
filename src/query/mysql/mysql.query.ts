a96ba897f07e5873d70c8f7dd34f974219feef6f
Udhaya-Phx <udhaya@phoenixtechnologies.io>
2025-03-27 16:11:55 +0530

    sql changes

--- src/query/mysql/mysql.query.ts
+++ src/query/mysql/mysql.query.ts
@@ -1,5 +1,5 @@
 export const updateMissingTransaction = () => {
-  return `UPDATE missing_transaction 
+  return `UPDATE missing_transaction_test 
   SET charge_query = ?,
   charge_event_query = ?,
   execution_status = ?
@@ -7,5 +7,5 @@ export const updateMissingTransaction = () => {
 };
 
 export const getMissingTransaction = (status: string) => {
-  return `SELECT * FROM missing_transaction WHERE execution_status = '${status}'`;
+  return `SELECT * FROM missing_transaction_test WHERE execution_status = '${status}'`;
 };
